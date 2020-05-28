a458b623db7402ec5bc7cae91b06e2bbf34e3cfa
shashank524 <40920035+shashank524@users.noreply.github.com>
2020-05-28 19:56:38 +0530

    Add files via upload

--- Abnormal_Event_Detection/start_live_feed.py
+++ Abnormal_Event_Detection/start_live_feed.py
@@ -5,58 +5,15 @@ from scipy.misc import imresize
 from test import mean_squared_loss
 from keras.models import load_model
 import argparse
+import os
+from event_recognition_api import check_for_anomalies
 
 
 
-parser=argparse.ArgumentParser()
-
-parser.add_argument('modelpath',type=str)
-
-args=parser.parse_args()
-
-modelpath=args.modelpath
+video=cv2.VideoCapture(0)
 
 
-vc=cv2.VideoCapture(0)
-rval=True
-print('Loading model')
-model=load_model(modelpath)
-print('Model loaded')
-
-threshold=0.1
-while True:
-	imagedump=[]
-	
-	for i in range(10):
-		rval,frame=vc.read()
-		frame=imresize(frame,(227,227,3))
-
-		#Convert the Image to Grayscale
-
-
-		gray=0.2989*frame[:,:,0]+0.5870*frame[:,:,1]+0.1140*frame[:,:,2]
-		gray=(gray-gray.mean())/gray.std()
-		gray=np.clip(gray,0,1)
-		imagedump.append(gray)
-
-
-	imagedump=np.array(imagedump)
-
-	imagedump.resize(227,227,10)
-	imagedump=np.expand_dims(imagedump,axis=0)
-	imagedump=np.expand_dims(imagedump,axis=4)
-
-
-	print('Processing data')
-
-	output=model.predict(imagedump)
-
-
-
-	loss=mean_squared_loss(imagedump,output)
-
-
-	if loss>threshold:
-		print('Anomalies Detected')
-
+while video.isOpened:
+	result = check_for_anomalies(video)
+	print(result)
 
